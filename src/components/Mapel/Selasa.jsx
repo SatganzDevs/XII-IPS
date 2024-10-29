38f9d1cc77322f8b55d382b7b40ad53f50a209e4
Kurniawan Satria <satganzdevs@gmail.com>
2024-10-29 23:47:15 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,34 +2,46 @@ const Selasa = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Matematika</div>
-					<div className="flex justify-center items-center text-sm">07.00-08.20</div>
+					<div className="w-[50%] text-base  font-medium">Ekonomi</div>
+					<div className="flex justify-center items-center text-sm">07.15-08.35</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">TJKT Linux</div>
-					<div className="flex justify-center items-center text-sm">08.20-09.40</div>
+					<div className="w-[50%] text-base  font-medium">Bahasa Indonesia</div>
+					<div className="flex justify-center items-center text-sm">08.35-09.55</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
 					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">09.40-10.00</div>
+					<div className="flex justify-center items-center text-sm">09.50-10.10</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">TJKT Mikrotik</div>
-					<div className="flex justify-center items-center text-sm">10.00-11.20</div>
+					<div className="w-[50%] text-base  font-medium">Matematika</div>
+					<div className="flex justify-center items-center text-sm">10.10-11.30</div>
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">Istirahat</div>
-					<div className="flex justify-center items-center text-sm">11.20-12.20</div>
+					<div className="w-[50%] text-base  font-medium">Senibudaya</div>
+					<div className="flex justify-center items-center text-sm">11.30-12.10</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">TJKT Mikrotik</div>
-					<div className="flex justify-center items-center text-sm">12.20-13.40</div>
+					<div className="w-[50%] text-base font-medium">Istirahat</div>
+					<div className="flex justify-center items-center text-sm">12.10-12.40</div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
-					<div className="w-[50%] text-base font-medium">PKK</div>
-					<div className="flex justify-center items-center text-sm">13.40-15.00</div>
+					<div className="w-[50%] text-base font-medium">Senibudaya</div>
+					<div className="flex justify-center items-center text-sm">12.40-13.20</div>
 				</div>
+
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Prakarya</div>
+					<div className="flex justify-center items-center text-sm">12.40-14.00</div>
+				</div>
+
+				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="1000">
+					<div className="w-[50%] text-base font-medium">Aqidah Akhlak</div>
+					<div className="flex justify-center items-center text-sm">14.00-15.20</div>
+				</div>
+
+				
         </>
     )
 } 
